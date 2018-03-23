5da52ad6d45a3d7b4659e386131fccfab9ba2949
Nicolas Kovacs <info@microlinux.fr>
2018-03-23 07:52:22 +0100

    Passer le système en anglais.

--- postinstall.sh
+++ postinstall.sh
@@ -105,6 +105,15 @@ echo -e "[${VERT}OK${GRIS}] \c"
 sleep $DELAY
 echo
 
+# Passer le système en anglais
+echo "::"
+echo -e ":: Passer le système en anglais... \c"
+sleep $DELAY
+localectl set-locale LANG=en_US.UTF8
+echo -e "[${VERT}OK${GRIS}] \c"
+sleep $DELAY
+echo
+
 echo
 
 exit 0
